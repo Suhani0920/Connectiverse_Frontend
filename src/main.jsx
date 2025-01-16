4c0cd41bfd9eba656124e2e65f8402fd013b5c05
suhani <suhani9620@gmail.com>
2025-01-16 18:19:22 +0530

    Initial setup: Added React app structure with lazy-loaded routes and Material-UI integration

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App'; // Make sure you import your App component
+import { CssBaseline } from '@mui/material';
+import { HelmetProvider } from 'react-helmet-async';
+// Create the root and render the app
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <CssBaseline /> {/* Include CssBaseline here */}
+    <App />
+  </StrictMode>
+);
