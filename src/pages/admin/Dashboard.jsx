06a9c4d7dd1f875d0d7b66cfaa8093841e5ca95a
suhani <suhani9620@gmail.com>
2025-08-20 22:09:46 +0530

    feat(Dashboard): update widget components to include icons and enhance styling for better visual representation

--- src/pages/admin/Dashboard.jsx
+++ src/pages/admin/Dashboard.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import AdminLayout from '../../components/layout/AdminLayout'
 import { Box, Container, Paper, Stack ,Typography} from '@mui/material'
-import { AdminPanelSettings, Group, Notifications, Person } from '@mui/icons-material'
+import { AdminPanelSettings, Group, Message, Notifications, Person } from '@mui/icons-material'
 import moment from 'moment'
 import { CurveButton, SearchField } from '../../components/styles/StyledComponents'
 
@@ -48,9 +48,9 @@ const Dashboard = () => {
  alignItems={"center"}
  margin={"2rem 0"}
  >
-     <Widget title={"Users"} value={34} Icon={Person}/>
-     <Widget title={"Chats"} value={3}/>
-     <Widget title={"Messages"} value={64}/>
+     <Widget title={"Users"} value={34} Icon={<Person/>}/>
+     <Widget title={"Chats"} value={3} Icon={<Group/>}/>
+     <Widget title={"Messages"} value={64} Icon={<Message/>}/>
 
  </Stack>
   return (
@@ -117,12 +117,30 @@ const Dashboard = () => {
   );
 };
 const Widget = ({title,value,Icon}) => {
- <Paper>
+ <Paper
+ sx={{
+  padding:"2rem",
+  margin:"2rem 0",
+  borderRadius:"1rem",
+  width:"20rem"
+ }}
+ >
   <Stack alignItems={"center"} spacing={"1rem"}>
-    <Typography>{value}</Typography>
+    <Typography
+    sx={{
+      color:"rgba(0,0,0,0.7)",
+      borderRadius:"50%",
+      border:`5px solid rgba(0,0,0,0.9)`,
+      width:"5rem",
+      height:"5rem",
+      display:"flex",
+      alignItems:"center",
+      justifyContent:"center"
+    }}
+    >{value}</Typography>
     <Stack>
       {Icon}
-     <Typography>{Title}</Typography> 
+     <Typography>{title}</Typography> 
     </Stack>
   </Stack>
   </Paper>;
